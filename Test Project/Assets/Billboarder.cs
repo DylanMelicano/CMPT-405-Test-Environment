e7a8e466f9e7de5334bba61ed8599951777d5335
Graeme <firewarrior2112@gmail.com>
2016-01-11 23:58:52 -0600

    added a bunch of textures and a creepy ass room

---
+++ Test Project/Assets/Billboarder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour
+{
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(90, player.transform.localEulerAngles.y - 180, 0);
+    }
+}
