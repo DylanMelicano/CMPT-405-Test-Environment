f3d9e2cf9bebf2b4f607309d0a1d2974422b963a
DylanMelicano <dylan_027@yahoo.com>
2016-01-15 16:08:57 -0600

    Added Prefabs for the 4th Area/Arc of the maze
    Added some of the prefabs for the last area of the maze and changed the
    camera movement script to only take x axis from mouse

--- Test Project/Assets/Scripts/CameraMovement.cs
+++ Test Project/Assets/Scripts/CameraMovement.cs
@@ -33,7 +33,7 @@ public class CameraMovement : MonoBehaviour
 
     void Update()
     {
-        if (axes == RotationAxes.MouseXAndY)
+        /*if (axes == RotationAxes.MouseXAndY)
         {
             float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 
@@ -43,16 +43,16 @@ public class CameraMovement : MonoBehaviour
             transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
         }
         else if (axes == RotationAxes.MouseX)
-        {
+        {*/
             transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-        }
+        /*}
         else
         {
             rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
             rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
 
             transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-        }
+        }*/
     }
 
     void Start()
