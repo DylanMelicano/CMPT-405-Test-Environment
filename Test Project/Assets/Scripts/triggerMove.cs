9307170270d4845afab65bfcbe1beb3c54dbf064
Graeme <firewarrior2112@gmail.com>
2016-01-19 00:32:28 -0600

    added first monster
    added movement script for him

---
+++ Test Project/Assets/Scripts/triggerMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerMove : MonoBehaviour {
+    public float activeTime;
+    public float speed;
+
+
+    private bool triggered = false;
+    private float timeSincetriggered;
+    
+    void Update()
+    {
+        if (triggered == true)
+        {
+            Debug.Log("hello");
+            transform.position += new Vector3(0, 0, -speed);
+            if ((Time.time - timeSincetriggered) > activeTime)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        
+    }
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            triggered = true;
+            timeSincetriggered = Time.time;
+        }
+    }
+}
