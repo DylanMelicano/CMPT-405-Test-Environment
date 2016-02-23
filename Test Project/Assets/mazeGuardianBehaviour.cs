b7487885326adaf92b50f8623ca2c2b8d6e37274
Graeme <firewarrior2112@gmail.com>
2016-02-23 12:45:23 -0600

    added maze enemy
    added the maze guardian, made him a clone of the pillar guardian, except
    i used iTween to animate him in a loop

---
+++ Test Project/Assets/mazeGuardianBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mazeGuardianBehaviour : MonoBehaviour {
+
+    GameObject m_Camera;
+    // Use this for initialization
+    void Start () {
+        m_Camera = GameObject.FindWithTag("MainCamera");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * -Vector3.up,
+                m_Camera.transform.rotation * -Vector3.forward);
+    }
+}
---
+++ Test Project/Assets/mazeGuardianMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mazeGuardianMovement : MonoBehaviour
+{
+
+    void Start()
+    {
+        iTween.MoveFrom(gameObject, iTween.Hash("path", iTweenPath.GetPath("guardian"), "time", 12, "looptype", "loop", "easetype", "linear"));
+    }
+}
+
