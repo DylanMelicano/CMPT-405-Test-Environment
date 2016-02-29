411abaccd3c3b36d26823ee1aca7a4b61b611d5d
Graeme <firewarrior2112@gmail.com>
2016-02-29 13:53:16 -0600

    made the end guardian
    made the last enemy in the game, he guards the last rooms of the game,
    walking towards the player if he gets too close

---
+++ Test Project/Assets/FollowPlayerWithinRange.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayerWithinRange : MonoBehaviour {
+
+    public Camera m_Camera;
+    public int speed;
+    public GameObject ProximityDetector;
+
+    ProximityDetector proxDet;
+    bool close = false;
+    GameObject parent;
+
+    // Use this for initialization
+    void Start()
+    {
+        proxDet = ProximityDetector.GetComponent<ProximityDetector>();
+
+        parent = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        close = proxDet.close;
+        parent.transform.LookAt(parent.transform.position + m_Camera.transform.rotation * Vector3.up,
+            m_Camera.transform.rotation * -Vector3.forward);
+        if(close == true)
+        {
+            parent.transform.position = Vector3.MoveTowards(parent.transform.position, m_Camera.transform.position, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Test Project/Assets/ProximityDetector.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProximityDetector : MonoBehaviour {
+
+    public bool close = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            close = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            close = false;
+        }
+    }
+}
